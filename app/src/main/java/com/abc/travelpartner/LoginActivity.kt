222fdf4c916875bbca4375ba15db53d02286fcd4
Govinda Eka Putra <73218633+govindaekaputra@users.noreply.github.com>
2021-06-07 13:47:39 +0800

    create login function

--- app/src/main/java/com/abc/travelpartner/LoginActivity.kt
+++ app/src/main/java/com/abc/travelpartner/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.abc.travelpartner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.abc.travelpartner.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -12,5 +17,35 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogin.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun checkLoggedInState() {
+        if(firebaseAuth.currentUser == null) {
+            Toast.makeText(this,"You are Logged In", Toast.LENGTH_SHORT).show()
+        }else {
+            Toast.makeText(this, "You are not Logged In yet", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loginUser() {
+        val email = binding.etEmailForm.text.toString()
+        val password = binding.etPasswordForm.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+            checkLoggedInState()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLoggedInState()
     }
 }
