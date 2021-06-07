66ab54d44bd9f7a1a11e7bfcac2fe09d97629163
Govinda Eka Putra <73218633+govindaekaputra@users.noreply.github.com>
2021-06-07 13:53:17 +0800

    create register function

--- app/src/main/java/com/abc/travelpartner/RegisterActivity.kt
+++ app/src/main/java/com/abc/travelpartner/RegisterActivity.kt
@@ -3,8 +3,18 @@ package com.abc.travelpartner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.abc.travelpartner.databinding.ActivityRegisterBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var userCollection: CollectionReference = db.collection("users")
 
     private lateinit var binding: ActivityRegisterBinding
 
@@ -12,5 +22,27 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegister.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val email = binding.etEmailForm.text.toString()
+        val password = binding.etPasswordForm.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                        override fun onComplete(task: Task<AuthResult>) {
+                            if(task.isComplete) {
+                                val currentUser = firebaseAuth.currentUser!!
+                                val currentUserId = currentUser.uid
+                                val userName = "@bambang123"
+                                userCollection.add(User(currentUserId,userName))
+                            }
+                        }
+                    })
+        }
     }
 }
