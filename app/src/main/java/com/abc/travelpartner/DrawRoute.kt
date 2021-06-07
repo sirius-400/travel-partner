b701e06fa30f67dba1162f7f6888e595c69006dc
Govinda Eka Putra <73218633+govindaekaputra@users.noreply.github.com>
2021-06-07 20:41:54 +0800

    create function to draw route in map

---
+++ app/src/main/java/com/abc/travelpartner/DrawRoute.kt
@@ -0,0 +1,22 @@
+package com.abc.travelpartner
+
+import android.graphics.Color
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
+
+class DrawRoute {
+    companion object{
+        private val path : MutableList<List<LatLng>> = arrayListOf()
+        fun drawRoute(googleMap: GoogleMap, route: RoutesItem){
+            route.legs?.get(0)?.steps?.forEach({
+                val point = it?.polyline?.points
+                path.add(PolyUtil.decode(point))
+            })
+            for(i in 0 until path.size){
+                googleMap.addPolyline(PolylineOptions().addAll(path[i]).color(Color.BLUE))
+            }
+        }
+    }
+}
