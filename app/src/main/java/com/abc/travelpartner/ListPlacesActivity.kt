04fafff71f988d8638a68c628b1b46b563c788fa
Govinda Eka Putra <73218633+govindaekaputra@users.noreply.github.com>
2021-06-08 21:58:32 +0800

    create search menu in list places activity

--- app/src/main/java/com/abc/travelpartner/ListPlacesActivity.kt
+++ app/src/main/java/com/abc/travelpartner/ListPlacesActivity.kt
@@ -2,10 +2,33 @@ package com.abc.travelpartner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
+import com.abc.travelpartner.databinding.ActivityListPlacesBinding
 
 class ListPlacesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityListPlacesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_places)
+        binding = ActivityListPlacesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.list_places_menu,menu)
+        val search = menu?.findItem(R.id.search_icon)
+        val searchView = search?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        })
+        return true
     }
 }
